4f7c626c91b04a9085f4f7a91eecda41a2754f72
tezosmiami <tezosmiami@gmail.com>
2022-11-03 13:42:35 -0400

    cors. . .

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const { Server } = require('ws');
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
 const server = express()
+    .use(cors({
+        origin: [
+        'https://www.henmiami.xyz', 'https://www.hicetnunc.miami',
+        'https://www.hic.miami','https://www.hen.miami',
+        'https://henmiami.xyz/', 'https://hicetnunc.miami',
+        'https://hic.miami','https://hen.miami'
+        ]
+    }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
