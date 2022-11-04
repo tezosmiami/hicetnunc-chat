9e4778904ea5fc8b3d30331d2b6fb9e3f6014510
tezosmiami <tezosmiami@gmail.com>
2022-11-03 21:55:57 -0400

    includes. . .

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ wss.on('connection', (socket) => {
                 }
              const online = {body:[] }
                 for (user of users.values()){
-                     user.alias!== userRef.alias && online.body.push(user.alias+' : ')  
+                     user.alias!== userRef.alias && !online.includes(user.alias) 
+                        && online.body.push(user.alias+' : ')  
                 }
                 sendMessage(online, socket)
                 sendMessage(messageToSend)
